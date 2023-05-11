ec75e949346a56d232a237602f1e1bc67e96e1bb
yesiambao <yesiambao@yesiamMacBook.lan>
2023-05-11 10:31:15 +0700

    feat: CI lint

--- main.go
+++ main.go
@@ -10,15 +10,6 @@ func index(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "Hello, yes I am Bao\n")
 }
 
-func headers(w http.ResponseWriter, req *http.Request) {
-
-	for name, headers := range req.Header {
-		for _, h := range headers {
-			fmt.Fprintf(w, "%v: %v\n", name, h)
-		}
-	}
-}
-
 func main() {
 
 	http.HandleFunc("/", index)
